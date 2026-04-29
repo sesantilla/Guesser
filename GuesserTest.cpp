a8f8f573b617842bb715a7582bf60f8a397e4c67
Skye Santilla <sesantilla@csuchico.edu>
2026-04-29 11:31:56 -0700

    Added more test cases - All cases seem to pass

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -44,4 +44,50 @@ TEST(GuesserTest, DistCap_SLength)
   ASSERT_EQ(object.distance("CompletelyDifferent"), 6);
 }
 
+TEST(GuesserTest, Constr_ScrtLngr_32Char)
+{
+  Guesser object("12345678901234567890123456789012EXTRA");
+  ASSERT_TRUE(object.match("12345678901234567890123456789012"));
+}
+
+TEST(GuesserTest, MatchRtrn_T_CrrtGuess)
+{
+  Guesser object("Secret");
+  ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, MatchRtrn_F_WrngGuess)
+{
+  Guesser object("Secret");
+  ASSERT_FALSE(object.match("Secrex"));
+}
+
+TEST(GuesserTest, ThreeWrng_Guess_Close)
+{
+  Guesser object("Secret");
+
+  ASSERT_FALSE(object.match("Secrex"));
+  ASSERT_FALSE(object.match("Secrey"));
+  ASSERT_FALSE(object.match("Secrez"));
+
+  ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, BigDist_LocksGuesser)
+{
+  Guesser object("Secret");
+
+  ASSERT_FALSE(object.match("Wrong"));
+  ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, CorrGuess_NoUse_RmainGuess)
+{
+  Guesser object("Secret");
+
+  ASSERT_TRUE(object.match("Secret"));
+  ASSERT_TRUE(object.match("Secret"));
+  ASSERT_TRUE(object.match("Secret"));
+  ASSERT_TRUE(object.match("Secret"));
+}
 
